deac548ea0b99e9706c5a48b8f66b71eed7e074a
Luan Ta <luan@tinypulse.com>
2018-05-20 17:23:02 +0700

    Add Artile controller rspec test

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,26 +1,85 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe ArticlesController, type: :controller do
-  before do
-  end
+  render_views
 
-  context 'when there is no filter param' do
-    it 'return list of all articles' do
+  describe 'GET index' do
+    context 'when there is no filter param and no articles in DB' do
+      it 'returns 200' do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns include 2 elements' do
+        get :index
+        expect(response.body).to include 'articlesCount'
+        expect(response.body).to include 'articles'
+      end
+
+      it 'returns empty list of articles when there is no any article' do
+        get :index, 'author' => 'Luan', 'article' => {}
+        expect(JSON[response.body]['articles']).to eq []
+      end
     end
-  end
 
-  context 'when params[:author] is exit' do
-    it 'return list articles filtered by author name in param' do 
+    context 'when there is no filter and have articles in DB' do
+      let!(:user1) { User.create(name: 'Luan Ta') }
+      let!(:user2) { User.create(name: 'Ngoc Nguyen') }
+      let!(:article1) { Article.create(user_id: User.first.id) }
+      let!(:article2) { Article.create(user_id: User.second.id) }
+
+      it 'returns list all articles' do
+        get :index
+        value = JSON[response.body]['articles']
+        expect(value.count).to eq 2
+        expect(value.first['id']).to eq article1.id
+        expect(value.second['id']).to eq article2.id
+      end
     end
-  end
 
-  context 'when params[:favorited] is exit' do
-    it 'return list articles that favorited by username in params' do 
+    context 'when there is filter by author' do
+      let!(:user1) { User.create(name: 'Luan 1') }
+      let!(:user2) { User.create(name: 'Luan 2') }
+      let!(:article1) { Article.create(user_id: User.first.id) }
+      let!(:article2) { Article.create(user_id: User.second.id) }
+
+      it 'returns list articles filtered by author name in param' do
+        get :index, author: 'Luan'
+        value = JSON[response.body]['articles']
+        expect(value.count).to eq 2
+        expect(value.first['id']).to eq article1.id
+        expect(value.second['id']).to eq article2.id
+      end
+
+      it 'returns correct articles filtered by author name' do
+        get :index, author: ' 1'
+        value = JSON[response.body]['articles']
+        expect(value.count).to eq 1
+        expect(value.first['id']).to eq article1.id
+      end
+
+      it 'returns list articles that favorited by username in params' do
+        get :index, author: 'not matched any'
+        value = JSON[response.body]['articles']
+        expect(value.count).to eq 0
+      end
+
+      it 'return correct list article even there are spaces before/after params' do
+        get :index, author: '           1         '
+        value = JSON[response.body]['articles']
+        expect(value.count).to eq 1
+        expect(value.first['id']).to eq article1.id
+      end
     end
-  end
 
-  context 'when params[:tag] is exit' do
-    it 'return list article that have tag text as in param' do 
+    context 'when there is filter by favorited' do
+      it 'TO DO'
+    end
+
+    context 'when there is filter by tag' do
+      it 'TO DO'
     end
   end
 end
